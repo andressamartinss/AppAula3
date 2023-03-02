c0ca0d82e99fe27a138fe92c98aef4931cd08689
andressamartinss <andressa.martins38@etec.sp.gov.br>
2023-03-02 09:01:57 -0300

    comentario

--- app/src/main/java/com/example/appaula/MainActivity.kt
+++ app/src/main/java/com/example/appaula/MainActivity.kt
@@ -12,9 +12,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val edtNome:EditText = findViewById(R.id.edtNome)
+        val edtEnde:EditText = findViewById(R.id.edtEnde)
+        val edtBai:EditText = findViewById(R.id.edtBai)
+        val edtCe:EditText = findViewById(R.id.edtCe)
+        val edtCi:EditText = findViewById(R.id.edtCi)
+        val edtEs:EditText = findViewById(R.id.edtEs)
         val btnCadastrar:Button = findViewById(R.id.btnCadastrar)
         btnCadastrar.setOnClickListener {
-            val toast = Toast.makeText(  this, edtNome.text, Toast.LENGTH_SHORT)
+            val texto:String = edtNome.text.toString() + edtEnde.text.toString() + edtBai.text.toString() + edtCe.text.toString()+ edtCi.text.toString()+ edtEs.text.toString()
+            val toast = Toast.makeText(  this,texto, Toast.LENGTH_SHORT)
             toast.show()
         }
     }
